2c6dfc78f38d781bb74ac2bed463b52600766027
José David Marroquín Toledo <jose@marroquin.cl>
2016-08-27 10:57:48 -0300

    Update code based on the Jupyter modules of S3, delete the illuminated object by default.

--- blend-scripts/ledgridbpy.py
+++ blend-scripts/ledgridbpy.py
@@ -1,72 +1,7 @@
 import bpy
 from mathutils import Matrix
 import math
-
-# Create a LED grid with a camera and a sample to be illuminated.
-def create_led_grid(sc, **kwargs):
-    RADIUS_LED_CYL = 4
-    DEPTH_LED_CYL = 0
-    SAMPLE_HEIGHT = 100
-    SCALE_SAMPLE = 2
-    h_leds = kwargs.pop('h_leds', 8)  # It is equivalent to the number of
-                                      # columns of the LED grid.
-    v_leds = kwargs.pop('v_leds', 8)  # ... to the number of the rows.
-    dist = kwargs.pop('dist', 10)  # Distance between LEDs.
-    # The LED grid will be centered in the (x, y, z).
-    x = kwargs.pop('x', 0)  # X coordinate in the x-axis.
-    y = kwargs.pop('y', 0)  # Y coordinate in the y-axis.
-    z = kwargs.pop('z', 40) # Z coordinate in the z-axis.
-    led_grid_coord = list()  # List of tuples with all coordinates of the
-                             # the LEDs.
-    # Determination of the limits of the grid.
-    h_limit_x = (h_leds * dist) / 2
-    c_limit_x = -h_limit_x
-    h_limit_y = (h_leds * dist) / 2
-    c_limit_y = -h_limit_y
-    h_limit_x_offset = h_limit_x + x
-    c_limit_x_offset = c_limit_x + x
-    h_limit_y_offset = h_limit_y + y
-    c_limit_y_offset = c_limit_y + y
-    # (x_init, y_init) as below represents the coordinate for the first LED in
-    # the x-plane and the y-plane.
-    x_init = c_limit_x_offset
-    y_init = c_limit_y_offset
-    # Fill the led_grid_coord ('list').
-    for i in range(h_leds):
-        for j in range(v_leds):
-            led_grid_coord.append((x_init, y_init))
-            x_init += dist
-        x_init = c_limit_x_offset
-        y_init += dist
-    for i in range(len(led_grid_coord)):
-        x_i, y_i = led_grid_coord[i]
-        lamp_name = "lamp" + str(i + 1)
-        # The lamp as cone.
-        lamp_data = bpy.data.lamps.new(name=lamp_name, type='SPOT')
-        lamp_data.energy = 0.0 # The lamp if off.
-        lamp_object = bpy.data.objects.new(name=lamp_name,
-                                           object_data=lamp_data)
-        sc.objects.link(lamp_object)
-        lamp_object.location = (x_i, y_i, z)
-        lamp_object.select = True
-        sc.objects.active = lamp_object
-        # Base model for the LED.
-        bpy.ops.mesh.primitive_cylinder_add(location=(x_i, y_i, z),
-                                            radius=RADIUS_LED_CYL,
-                                            depth=DEPTH_LED_CYL)
-    # Create the camera.
-    cam_name = "camgrid"
-    cam_data = bpy.data.cameras.new(name=cam_name)
-    cam_object = bpy.data.objects.new(name=cam_name, object_data=cam_data)
-    scene.objects.link(cam_object)
-    cam_object.location = (x, y, z - 1)
-    sc.objects.active = cam_object
-    # The sample.
-    bpy.ops.mesh.primitive_cylinder_add(location=(x, y, 0),
-                                        radius=RADIUS_LED_CYL * SCALE_SAMPLE,
-                                        depth=SAMPLE_HEIGHT,
-                                        rotation=(math.radians(90), 0, 0))
-    return cam_object
+import os
 
 # Delete all objects of the current blend file.
 def delete_all_objects():
@@ -75,45 +10,105 @@ def delete_all_objects():
         o.select = True
     bpy.ops.object.delete()  # Delete the selected objects.
 
-# Render a scene (bpy.context.scene), sc, with a camera (bpy.types.Camera),
+# Return the number of LEDs per row and column, the camera ('bpy.types.Camera')
+# and the list with (x, y, z) coordinates 'tuple') of the all LED of the grid.
+def create_led_grid(sc, **kwargs):
+    rows = kwargs.pop('rows', 15)  # Number of LEDs per row and column.
+    dist = kwargs.pop('dist', 6)  # Distance in mm between LEDs.
+    # The LED grid will be centered in the (x, y, z).
+    xgrid = kwargs.pop('xgrid', 0)  # X coordinate of the grid in the x-axis.
+    ygrid = kwargs.pop('ygrid', 0)  # ... Y in the y-axis.
+    zgrid = kwargs.pop('zgrid', 40) # ... Z in the z-axis.
+    radiusled = kwargs.pop('radiusled', 2.5)  # Radius in mm of the LED.
+    led_grid_coord = list()  # List of tuples with all coordinates of the LEDs
+                             # of the grid.
+    # Determination of the limits of the grid when it is in the center.
+    plimit_x = (rows * dist) / 2  # Positive horizontal limit for the X
+                                  # coordinate.
+    nlimit_x = -plimit_x  # Negative horizontal limit for the X coordinate.
+    plimit_y = (rows * dist) / 2
+    nlimit_y = -plimit_y
+    # Determination of the limits of the grid when it is moved in x-axis and
+    # y-axis.
+    plimit_x_moved = plimit_x + xgrid
+    nlimit_x_moved = nlimit_x + xgrid
+    plimit_y_moved = plimit_y + ygrid
+    nlimit_y_moved = nlimit_y + ygrid
+    x_init = nlimit_x_moved  # X coordinate of the first LED of the grid.
+    y_init = plimit_y_moved  # Y coordinate...
+    # Fill the led_grid_coord ('list') with the coordinates of the LEDs.
+    for i in range(rows):
+        for j in range(rows):
+            led_grid_coord.append((x_init, y_init, zgrid))
+            x_init += dist
+        x_init = nlimit_x_moved
+        y_init -= dist
+    for i in range(len(led_grid_coord)):
+        x_i, y_i, z_i = led_grid_coord[i]
+        lamp_name = "lamp" + str(i + 1)
+        # The lamp as a cone.
+        lamp_data = bpy.data.lamps.new(name=lamp_name, type='SPOT')
+        lamp_data.energy = 0.0 # The lamp if off.
+        lamp_object = bpy.data.objects.new(name=lamp_name,
+                                           object_data=lamp_data)
+        sc.objects.link(lamp_object)
+        lamp_object.location = (x_i, y_i, z_i)
+        lamp_object.select = True
+        sc.objects.active = lamp_object
+        # Base model for the LED.
+        bpy.ops.mesh.primitive_cylinder_add(location=(x_i, y_i, z_i),
+                                            radius=radiusled,
+                                            depth=0)
+    # Create the camera.
+    cam_name = "camgrid"
+    cam_data = bpy.data.cameras.new(name=cam_name)
+    cam_object = bpy.data.objects.new(name=cam_name, object_data=cam_data)
+    scene.objects.link(cam_object)
+    cam_object.location = (xgrid, ygrid, zgrid - 1)
+    sc.objects.active = cam_object
+    # cam_object ('bpy.types.Camera') MUST BE RETURNED to render the scene.
+    return rows, cam_object, led_grid_coord
+
+# "Return a string that contains a sequence n-zeros followed by num ('int') as
+# 'str', for example, num_str_zeros(89, 4) returns '0089'". From
+# fwdimaging.ipynb Jupyter notebook.
+def num_str_zeros(num, n_digs, matlab=False):
+    if matlab:  # Begin the numeration with 1 in the filename.
+        num += 1
+    len_num = len(str(num))
+    str_num = ''
+    for i in range(n_digs - len_num):
+        str_num += '0'
+    str_num += str(num)
+    return str_num
+
+# Render a scene ('bpy.context.scene'), sc, with a camera ('bpy.types.Camera'),
 # cam, and save the result in render_path ('str') as PNG file.
-def take_photo(cam, sc, sufix, **kwargs):
-    if bpy.data.is_saved == False:
-        dir_name = 'tmp_output_render'
-    else:
-        dir_name = bpy.data.filepath[:-6] + '_output_render'
-    dest = kwargs.pop('dest', dir_name)
-    name = kwargs.pop('name', '')
+def take_photo(cam, sc, filename, outpath='img_lores/'):
     sc.camera = cam
-    dest += '/'
-    if name != '':
-        dest += '_'
-    dest += sufix
-    render_path = dest
-    # print(render_path)
-    sc.render.filepath = render_path
+    sc.render.filepath = outpath + filename
     bpy.ops.render.render(write_still=True)
 
-# Return a list ('list') with all lamps (bpy.types.Lamp) of the blend file.
-def get_lamps(**kwargs):
-    prefix = kwargs.pop('prefix', 'lamp')
+# Turn on a lamp at time, take a photo of the mesh and save it.
+def illuminate_step_by_step(rows, cam, sc, **kwargs):
+    prefix = kwargs.pop('prefix', 'lores_')
+    l_lamps = get_lamps()
+    for i in range(len(l_lamps)):
+        l_lamps[i].data.energy = 2.0
+        take_photo(cam, sc, prefix + num_str_zeros(i, len(str(rows ** 2)),
+                                                      matlab=True))
+        l_lamps[i].data.energy = 0.0
+
+# Return a list ('list') with all lamps ('bpy.types.Lamp') of the blend file.
+def get_lamps(prefix='lamp'):
     l_lamps = list()
     for o in bpy.data.objects:
         if prefix in o.name:
             l_lamps.append(o)
     return l_lamps
 
-# Turn on a lamp each time of the grid, take a photo of the mesh and save
-# it as PNG file.
-def illuminate_step_by_step(cam, sc):
-    l_lamps = get_lamps()
-    for i in range(len(l_lamps)):
-        l_lamps[i].data.energy = 2.0
-        take_photo(cam, sc, str(i + 1))
-        l_lamps[i].data.energy = 0.0
-           
 if __name__ == '__main__':
     delete_all_objects()
     scene = bpy.context.scene
-    cam = create_led_grid(scene)
-    illuminate_step_by_step(cam, scene)
+    rows, cam, _ = create_led_grid(scene)
+    illuminate_step_by_step(rows, cam, scene)
