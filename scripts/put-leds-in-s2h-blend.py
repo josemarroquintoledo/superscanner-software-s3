e971ae256a96d143bf1b8552caf428167d579f7a
José David Marroquín Toledo <jose@marroquin.cl>
2016-11-08 22:37:09 -0300

    Bug fix and change the sample path for the STL mesh.

--- scripts/put-leds-in-s2h-blend.py
+++ scripts/put-leds-in-s2h-blend.py
@@ -6,12 +6,12 @@ __status__ = "Development"
 import math
 import bpy
 import blendjupyter as blendj
-import illuminator as led
+import illuminatorbpy as led
 
 if __name__ == '__main__':
     leds = [12, 18, 36]
     l = led.gen_xyz_led_ring([12, 18, 36], 5, 0)
     parent_name = 'S2H-LED-ring-illuminator'
-    led_stl_path = '/home/marroquintoledo-l-br/Escritorio/LED_prop.stl'
-    blendj.put_mesh(led_stl_path, l, copies=sum(leds), rotation=(180, 0, 0),
+    stl_path = ''
+    blendj.put_mesh(stl_path, l, copies=sum(leds), rotation=(180, 0, 0),
                     parent=parent_name, offset=(0, 0, -1.51346))
